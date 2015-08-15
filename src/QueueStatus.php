fafc15cd7801bdf6163d399ec7d116ead260fe9e
Bart Nagel <bart@tremby.net>
2015-08-14 17:48:50 -0700

    Include diff in seconds in json output

--- src/QueueStatus.php
+++ src/QueueStatus.php
@@ -210,7 +210,9 @@ class QueueStatus implements
             'status' => $this->getStatus(),
             'message' => $this->getMessage(),
             'startTime' => $this->getStartTime(),
+            'startTimeDiff' => $this->getStartTime()->diffInSeconds(null, false),
             'endTime' => $this->getEndTime(),
+            'endTimeDiff' => $this->getEndTime()->diffInSeconds(null, false),
         ];
     }
 
