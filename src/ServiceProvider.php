f7356b5fbeb7ac99e3e11a38ff78153d66a812aa
Bart Nagel <bart@tremby.net>
2015-08-17 14:11:56 -0700

    Key queues in json response by queue name

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -27,14 +27,16 @@ class ServiceProvider extends BaseServiceProvider
 
         // Composer for the status views
         $composer = function ($view) {
-            $view->withQueues(array_map(function ($queueName) {
+            $queues = [];
+            foreach (Cache::get(QueueMonitor::QUEUES_CACHE_KEY, []) as $queueName) {
                 $status = QueueStatus::get($queueName);
                 if (!$status) {
                     $status = new QueueStatus($queueName, QueueStatus::ERROR, false);
                     $status->setMessage("Status not found in cache; is a cron job set up and running?");
                 }
-                return $status;
-            }, Cache::get(QueueMonitor::QUEUES_CACHE_KEY, [])));
+                $queues[$queueName] = $status;
+            }
+            $view->withQueues($queues);
         };
         View::composer('queue-monitor::status', $composer);
         View::composer('queue-monitor::status-json', $composer);
